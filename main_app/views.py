5c65f41c5cf9a81d34ead10e411891ab02b3b8b8
cpak125 <chris.pak125@gmail.com>
2022-06-09 14:04:22 -0400

    create Cat Model

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# Add the Cat class & list and view function below the imports
+class Cat:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+cats = [
+  Cat('Lolo', 'tabby', 'foul little demon', 3),
+  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
 
 # Create your views here.
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 
 # Define the about view
 def about(request):
   return render(request, 'about.html')
+
+# Define cats index view
+def cats_index(request):
+  return render(request, 'cats/index.html', {'cats': cats})
