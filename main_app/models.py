dc61010ee47fe6c7d1a596d37e0e9a2787e48240
cpak125 <chris.pak125@gmail.com>
2022-06-11 14:51:41 -0400

    implement create, update, delete functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Cat(models.Model):
@@ -10,3 +11,6 @@ class Cat(models.Model):
 
   def __str__(self):
     return f'{self.name} ({self.id})'
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'cat_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
 # Create your views here.
@@ -20,3 +21,19 @@ def cats_index(request):
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
   return render(request, 'cats/detail.html', {'cat': cat})
+
+# Class-Based View (CBV) to create a cat
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+
+# Class-Based View to update a cat 
+class CatUpdate(UpdateView):
+  model = Cat
+  # Let's disallow the renaming of act by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+# Class-Based View to delete a cat
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats/'
