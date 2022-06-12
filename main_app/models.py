1251e10a87ac7c5f1520414d98334d266dbff5d0
cpak125 <chris.pak125@gmail.com>
2022-06-11 21:57:05 -0400

    implement creating and reading feedings for a cat

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+# A tuple of 2-tuples
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+)
+
 # Create your models here.
 class Cat(models.Model):
   #define the fields/columns
@@ -8,9 +15,36 @@ class Cat(models.Model):
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  # there will be a 'feeding_set' related-manager
+  #  used to access a cat's feedings
 
   def __str__(self):
     return f'{self.name} ({self.id})'
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'cat_id': self.id})
+
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  # Create a cat_id FK column in DB table and it will also provide a way 
+  # to access the entire cat object that the feeding belongs to
+  cat = models.ForeignKey(
+    Cat,
+    on_delete=models.CASCADE
+  )
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+
+  # change the default sort
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
+from .forms import FeedingForm
 
 # Create your views here.
 
@@ -20,7 +21,26 @@ def cats_index(request):
 # Define cats detail view
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', {'cat': cat})
+  # instantiate FeedingForm to be rendered 
+  # within the detail.html template
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', {
+    'cat': cat,
+    'feeding_form': feeding_form
+  })
+
+# Define the add feeding for a cat functionality
+def add_feeding(request, cat_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)    
 
 # Class-Based View (CBV) to create a cat
 class CatCreate(CreateView):
